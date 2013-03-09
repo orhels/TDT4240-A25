9fe5c71602a4f2fd197f86fc5b29e232697a985f
Ørjan Breimo Helstrøm <orjan@dhcp-022251.wlan.ntnu.no>
2013-03-09 12:52:55 +0100

    Initial project creation

---
+++ AirHockey/src/tdt4240/A25/AirHockeyActivity.java
@@ -0,0 +1,15 @@
+package tdt4240.A25;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AirHockeyActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
