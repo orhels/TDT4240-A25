1553177c243ccc5008da9e5bec7b44a48d838272
Ørjan Breimo Helstrøm <orjan@dhcp-020245.wlan.ntnu.no>
2013-03-12 15:04:56 +0100

    Added a green screen

--- AirHockey/src/com/example/airhockey/MainActivity.java
+++ AirHockey/src/com/example/airhockey/MainActivity.java
@@ -1,47 +1,40 @@
 package com.example.airhockey;
 
-import org.andengine.engine.Engine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
 public class MainActivity extends SimpleBaseGameActivity {
 
 
-	private Camera mCamera;
+	static final int CAMERA_WIDTH = 800;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	@Override
-	public EngineOptions onCreateEngineOptions() {
-		this.mCamera = new Camera(0, 0, 480, 800);
-		this.mCamera.setCenter(0,0);
+    static final int CAMERA_HEIGHT = 480;
 
-		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(480, 800), this.mCamera);
-	}
+    @Override
 
-	@Override
-	protected void onCreateResources() {
-		// TODO Auto-generated method stub
-		
-	}
+    public EngineOptions onCreateEngineOptions() {
+    	Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    	return new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR,
+    			new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
 
-	@Override
-	protected Scene onCreateScene() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
+
+    @Override
+    protected void onCreateResources() {
+    	// TODO Auto-generated method stub
+    }
+
+    @Override
+    protected Scene onCreateScene() {
+    	Scene scene = new Scene();
+    	scene.setBackground(new Background(0.09804f, 0.6274f, 0));
+    	return scene;
+    }
+
 
 }
