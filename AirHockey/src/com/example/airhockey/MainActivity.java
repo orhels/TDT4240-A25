8aaa016593378fb34e1c01fe416cc062c7fa1849
Ørjan Breimo Helstrøm <orjan@dhcp-020245.wlan.ntnu.no>
2013-03-12 14:19:31 +0100

    Added some bullshit

--- AirHockey/src/com/example/airhockey/MainActivity.java
+++ AirHockey/src/com/example/airhockey/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.airhockey;
 
+import org.andengine.engine.Engine;
+import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
@@ -10,17 +14,22 @@ import android.view.Menu;
 
 public class MainActivity extends SimpleBaseGameActivity {
 
+
+	private Camera mCamera;
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		// TODO Auto-generated method stub
-		return null;
+		this.mCamera = new Camera(0, 0, 480, 800);
+		this.mCamera.setCenter(0,0);
+
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(480, 800), this.mCamera);
 	}
 
 	@Override
