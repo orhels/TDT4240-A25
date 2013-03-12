ba99d2581bd16d04301ef5fc9ec947036413324d
Ørjan Breimo Helstrøm <orjan@dhcp-020245.wlan.ntnu.no>
2013-03-12 13:19:41 +0100

    Fixed it! NOOBS!

---
+++ AirHockey/src/com/example/airhockey/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.airhockey;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends SimpleBaseGameActivity {
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
