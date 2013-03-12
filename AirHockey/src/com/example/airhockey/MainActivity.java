97a9d01aa9f4106bf2c8ec335cdacd18f2ba0c20
John Arne Øye <johnarneoye@gmail.com>
2013-03-12 14:04:31 +0100

    added menuscreen

--- AirHockey/src/com/example/airhockey/MainActivity.java
+++ AirHockey/src/com/example/airhockey/MainActivity.java
@@ -6,10 +6,21 @@ import org.andengine.ui.activity.SimpleBaseGameActivity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.view.Menu;
+import android.view.Window;
 
 public class MainActivity extends SimpleBaseGameActivity {
 
+	
+	@Override
+	protected void onCreate(Bundle pSavedInstanceState) {
+		super.onCreate(pSavedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		
+	}
+	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		// TODO Auto-generated method stub
@@ -21,7 +32,7 @@ public class MainActivity extends SimpleBaseGameActivity {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	protected Scene onCreateScene() {
 		// TODO Auto-generated method stub
