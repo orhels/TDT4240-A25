d90e654b672e38a5f8cd7a986b5353cc340f45bc
Ørjan Breimo Helstrøm <orjan@dhcp-020245.wlan.ntnu.no>
2013-03-12 11:56:26 +0100

    Added our package

---
+++ AirHockey/src/ntnu/AirHockeyMainActivity.java
@@ -0,0 +1,30 @@
+package ntnu;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+public class AirHockeyMainActivity extends SimpleBaseGameActivity {
+
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+	
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		
+		return null;
+	}
+
+	@Override
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
