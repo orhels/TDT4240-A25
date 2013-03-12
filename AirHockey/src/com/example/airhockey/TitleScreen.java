bf6e90a0dc7a2c8cd52f4c2311751b6732070d61
John Arne Øye <johnarneoye@gmail.com>
2013-03-12 15:49:53 +0100

    FUCKN TITLESCREEN UPD8!

--- AirHockey/src/com/example/airhockey/TitleScreen.java
+++ AirHockey/src/com/example/airhockey/TitleScreen.java
@@ -1,26 +1,59 @@
 package com.example.airhockey;
 
+import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.scene.menu.MenuScene;
 import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
 import org.andengine.entity.scene.menu.item.IMenuItem;
 import org.andengine.entity.util.FPSLogger;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
 public class TitleScreen extends SimpleBaseGameActivity implements IOnMenuItemClickListener {
 
 	Scene mainMenu;
+	
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+	
+	protected static final int MENU_NEWGAME = 0;
+	protected static final int MENU_HIGHSCORES = 1;
+	protected static final int MENU_SETTINGS = 2;
+	protected static final int MENU_QUIT = 3;
+	
+	protected Camera camera;
+	
+	private BitmapTextureAtlas menuTexture;
+	protected ITextureRegion titleTexture;
+	protected ITextureRegion newgameTexture;
+	protected ITextureRegion highscoreTexture;
+	protected ITextureRegion settingsTexture;
+	protected ITextureRegion quitTexture;
+	
+	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		// TODO Auto-generated method stub
-		return null;
+		this.camera = new Camera(0,0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.camera);
 	}
 
 	@Override
 	protected void onCreateResources() {
-		// TODO Auto-generated method stub
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("res/");
+		this.menuTexture = new BitmapTextureAtlas(this.getTextureManager(), 64, 64);
+		this.titleTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.menuTexture, this, ".png", 0,0);
+		this.newgameTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.menuTexture, this, ".png", 0,50);
+		this.highscoreTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.menuTexture, this, ".png", 0,100);
+		this.settingsTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.menuTexture, this, ".png", 0,150);
+		this.quitTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.menuTexture, this, ".png", 0,200);
+		this.menuTexture.load();
 		
 	}
 
