6dfde77e56a19837780c164f42f67f3bbb2d9315
John Arne Øye <johnarneoye@gmail.com>
2013-03-12 13:40:28 +0100

    dawd

--- AirHockey/src/com/example/airhockey/MainActivity.java
+++ AirHockey/src/com/example/airhockey/MainActivity.java
@@ -11,13 +11,6 @@ import android.view.Menu;
 public class MainActivity extends SimpleBaseGameActivity {
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
 	public EngineOptions onCreateEngineOptions() {
 		// TODO Auto-generated method stub
 		return null;
