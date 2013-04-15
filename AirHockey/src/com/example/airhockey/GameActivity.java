b8ceac74140b6b0e4eff3afe2c9fe2b7988cd5ed
John Arne Øye <johnarneoye@gmail.com>
2013-04-15 16:04:55 +0200

    multitouch stuff in gameactivity

--- AirHockey/src/com/example/airhockey/GameActivity.java
+++ AirHockey/src/com/example/airhockey/GameActivity.java
@@ -6,10 +6,12 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.input.touch.controller.MultiTouch;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.color.Color;
 
 import android.app.Activity;
+import android.widget.Toast;
 
 public class GameActivity extends SimpleBaseGameActivity {
 
@@ -27,10 +29,21 @@ public class GameActivity extends SimpleBaseGameActivity {
 	public EngineOptions onCreateEngineOptions() {
 		instance = this;
     	mCamera = new Camera(0, 0, Constants.CAMERA_WIDTH, Constants.CAMERA_HEIGHT);
-    	return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED,
+    	EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED,
     			new RatioResolutionPolicy(Constants.CAMERA_WIDTH, Constants.CAMERA_HEIGHT), mCamera);
+    	engineOptions.getTouchOptions().setNeedsMultiTouch(true);
+    	
+        if(MultiTouch.isSupported(this)) {
+            if(MultiTouch.isSupportedDistinct(this)) {
+                Toast.makeText(this, "MultiTouch detected --> Both controls will work properly!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "MultiTouch detected, but your device has problems distinguishing between fingers.\n\nControls are placed at different vertical locations.", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Sorry your device does NOT support MultiTouch!\n\n(Falling back to SingleTouch.)\n\nControls are placed at different vertical locations.", Toast.LENGTH_LONG).show();
+        }
 
-
+        return engineOptions;
 	}
 
 	@Override
